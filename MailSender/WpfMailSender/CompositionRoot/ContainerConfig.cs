3239907186d1ba64a1b5b9743a94415479fc8bee
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-12-24 20:49:40 +0300

    Implemented root composition with Autofac.

---
+++ MailSender/WpfMailSender/CompositionRoot/ContainerConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using RepositoryAbstract;
+using WpfMailSender.Abstracts;
+using WpfMailSender.ViewModels;
+using WpfMailSender.Views;
+
+namespace WpfMailSender.CompositionRoot
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindowViewModelFactory>().As<IMainWindowViewModelFactory>();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainWindowAdapter>().As<IWindow>();
+
+            return builder.Build();
+        }
+    }
+}
