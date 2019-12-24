a4f9d3a868239c4b6fa6716b08bbdad3e407d92e
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-12-24 20:38:47 +0300

    Added interfaces for dependency inversion.

---
+++ MailSender/WpfMailSender/Abstracts/IMainWindowViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMailSender.ViewModels;
+
+namespace WpfMailSender.Abstracts
+{
+    public interface IMainWindowViewModelFactory
+    {
+        MainWindowViewModel Create(IWindow window);
+    }
+}
