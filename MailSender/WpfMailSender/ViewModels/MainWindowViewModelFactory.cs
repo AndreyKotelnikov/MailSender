0e5596134d2a3c922ab52ef50af83f0e098eaaac
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-12-24 20:41:06 +0300

    Made implementation of interfaces for dependency inversion.

---
+++ MailSender/WpfMailSender/ViewModels/MainWindowViewModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryAbstract;
+using WpfMailSender.Abstracts;
+
+namespace WpfMailSender.ViewModels
+{
+    public class MainWindowViewModelFactory : IMainWindowViewModelFactory
+    {
+        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+        //TODO Сделать связку со слоем бизнесс-логики в VMFactory
+        //public MainWindowViewModelFactory(IUnitOfWorkFactory unitOfWorkFactory)
+        //{
+        //    _unitOfWorkFactory = unitOfWorkFactory;
+        //}
+
+
+        public MainWindowViewModel Create(IWindow window)
+        {
+            return new MainWindowViewModel(window);
+        }
+    }
+}
---
+++ MailSender/WpfMailSender/Views/MainWindowAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfMailSender.Abstracts;
+
+namespace WpfMailSender.Views
+{
+    public class MainWindowAdapter : IWindow
+    {
+        private readonly MainWindow _mainWindow;
+        private readonly IMainWindowViewModelFactory _vmFactory;
+
+        public MainWindowAdapter(MainWindow mainWindow, IMainWindowViewModelFactory vmFactory)
+        {
+            _mainWindow = mainWindow;
+            _vmFactory = vmFactory;
+
+            var vm = _vmFactory.Create(this);
+            _mainWindow.DataContext = vm;
+        }
+
+        void IWindow.Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        IWindow IWindow.CreateChild(object viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IWindow.Show() => _mainWindow.Show();
+        
+
+        bool? IWindow.ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
