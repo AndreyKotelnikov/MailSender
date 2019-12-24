0e749d95d5ec5730605b378dc1e6b9b2bdc17064
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-12-24 20:58:26 +0300

    In App.xaml.cs in the OnStartup method implemented a container call from CompositionRoot and MainWindow showing.

--- MailSender/WpfMailSender/App.xaml.cs
+++ MailSender/WpfMailSender/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
+using WpfMailSender.Abstracts;
+using WpfMailSender.CompositionRoot;
+
 
 namespace WpfMailSender
 {
@@ -13,5 +17,11 @@ namespace WpfMailSender
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var container = ContainerConfig.Configure();
+            container.Resolve<IWindow>().Show();
+        }
     }
 }
